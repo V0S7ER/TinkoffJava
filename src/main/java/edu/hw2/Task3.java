187a3333cf0188358339cd5c91b09ddf2dc430de
V0S7ER <k1ng.brown@yandex.ru>
2023-10-21 21:05:33 +0300

    HW2 main functional added

---
+++ src/main/java/edu/hw2/Task3.java
@@ -0,0 +1,98 @@
+package edu.hw2;
+
+import java.util.Random;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Task3 {
+    private static final Logger LOGGER = LogManager.getLogger();
+    private static final String SUCCESS_MESSAGE = "Success!";
+
+    public interface Connection extends AutoCloseable {
+        void execute(String command);
+    }
+
+    public static final class StableConnection implements Connection {
+
+        @Override public void execute(String command) {
+            LOGGER.info(SUCCESS_MESSAGE);
+        }
+
+        @Override public void close() {
+            // to do some hard things
+        }
+    }
+
+    public final class FaultyConnection implements Connection {
+        private static final double FAULT_PROBABILITY = 0.1;
+        private static final Random RANDOM = new Random();
+
+        @Override public void execute(String command) {
+            if (RANDOM.nextDouble() < FAULT_PROBABILITY) {
+                throw new ConnectionException();
+            }
+
+            LOGGER.info(SUCCESS_MESSAGE);
+        }
+
+        @Override public void close() {
+            // close
+        }
+    }
+
+    public interface ConnectionManager {
+        Connection getConnection();
+    }
+
+    public final class DefaultConnectionManager implements ConnectionManager {
+        private static final double FAULT_PROBABILITY = 0.1;
+        private static final Random RANDOM = new Random();
+
+        @Override public Connection getConnection() {
+            return RANDOM.nextDouble() < FAULT_PROBABILITY ? new FaultyConnection() : new StableConnection();
+        }
+    }
+
+    public final class FaultyConnectionManager implements ConnectionManager {
+
+        @Override public Connection getConnection() {
+            return new FaultyConnection();
+        }
+    }
+
+    public static class ConnectionException extends RuntimeException {
+    }
+
+    public static final class PopularCommandExecutor {
+        private final ConnectionManager manager;
+        private final int maxAttempts;
+
+        public PopularCommandExecutor(ConnectionManager manager, int maxAttempts) {
+            this.manager = manager;
+            if (maxAttempts <= 0) {
+                throw new IllegalArgumentException();
+            }
+            this.maxAttempts = maxAttempts;
+        }
+
+        public void updatePackages() throws Exception {
+            tryExecute("apt update && apt upgrade -y");
+        }
+
+        void tryExecute(String command) throws Exception {
+            ConnectionException cause = null;
+            for (int iteration = 0; iteration < maxAttempts; ++iteration) {
+                try (
+                    Connection connection = manager.getConnection()) { // try-with-resources - connection само закроется
+                    connection.execute(command);
+                    return; // если выполнилось - оно завершится тут
+                } catch (ConnectionException e) {
+                    cause = e;
+                }
+            }
+
+            assert cause != null;
+            throw cause; // если не выполнилось - кидаем исключение
+        }
+    }
+}
---
+++ src/main/java/edu/hw2/Task4.java
@@ -0,0 +1,14 @@
+package edu.hw2;
+
+public class Task4 {
+    private Task4() {
+    }
+
+    public static CallingInfo callingInfo() {
+        StackTraceElement traceElement =
+            new Throwable().getStackTrace()[0]; // первый элемент - это наша функция, т.к. мы в нее только вошли
+        return new CallingInfo(traceElement.getClassName(), traceElement.getMethodName());
+    }
+
+    public record CallingInfo(String className, String methodName) {}
+}
